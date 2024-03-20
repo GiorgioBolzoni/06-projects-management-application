23a5f20cd13087e2d3502ff699c1b5103a340042
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-20 10:50:52 +0100

    base Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <>
+    <Sidebar/>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
