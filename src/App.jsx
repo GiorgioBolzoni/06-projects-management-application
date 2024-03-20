42a219a3d0087a12b0fbf4ecb5548aeffc9ce47d
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-20 17:14:24 +0100

    NewProject impostazione iniziale

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+
+import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
     <Sidebar/>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen px-10 py-8 ms-72 overflow-hidden gap-8 w-2/3">
+        <NewProject/>
+      </main>
     </>
   );
 }
---
+++ src/components/NewProject.jsx
@@ -0,0 +1,31 @@
+export default function NewProject(){
+    return(
+        <>
+            <section className="border-b-2 border-stone-400 pb-10 mb-10">
+                <div className="flex justify-between items-center">
+                    <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+                    <button className="bg-stone-900 text-stone-50 rounded px-10 h-10 hover:bg-gradient-to-r from-blue-900 to-yellow-600">Delete</button>
+                </div>
+                <p className="text-stone-400 mb-4">
+                    data creazione mm gg,aaaa
+                </p>
+                <p>text1</p>
+                <p className="mt-8">text2</p>
+            </section>
+            <section className="w-dvw pb-10 mb-10">
+                <h3 className="text-4xl font-bold">
+                    Tasks
+                </h3>
+                <div className="my-8 flex gap-4">
+                    <input type="text" className="bg-stone-200 rounded border-black border-2 px-2 py-1 w-2/6" />
+                    <button htmlFor="Task" className="hover:text-red-500">Add Task</button>
+                </div>
+                <div className="my-8 flex">
+                    <textarea name="textarea" id="textarea" cols="30" rows="4" className="bg-stone-200 rounded-s border-black border-r-0 border-2 px-2 py-1 w-3/6"></textarea>
+                    <button htmlFor="Task" className="bg-stone-200 rounded border-black border-l-0 border-2 rounded-r px-2 hover:bg-stone-300">Clear</button>
+                </div>
+            </section>
+            
+        </>
+    )
+}
