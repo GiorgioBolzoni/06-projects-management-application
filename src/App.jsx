e7c2488552e23a05ae3494cbbd322431573181dc
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-22 17:06:55 +0100

    Managing State to Switch Between Components & Collecting User Input with Refs & Forwarded Refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
+import {useState} from 'react';
 
-import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
+import NoProjects from "./components/NoProjects";
+import NewProject from "./components/NewProject";
+// import Project from "./components/Project";
+
 
 function App() {
 
-  
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(
+      prevState => {
+        return{
+          ...prevState,
+          selectedProjectId: null,
+        }
+      }
+  )}
+
+    function handleAddProject(projectData){
+      setProjectsState(
+        prevState => {
+          const newProject = {
+            ...projectData,
+            id: Math.random(),
+          }
+          return{
+            ...prevState,
+            projects: [...prevState.projects, newProject]
+        }
+    })
+  }
+console.log(projectsState);
+      let content;
+
+      if(projectsState.selectedProjectId === null){
+        content = <NewProject onAdd={handleAddProject}/>;
+      } else if (projectsState.selectedProjectId === undefined){
+        content = <NoProjects onStartAddProject={handleStartAddProject} />;
+      }
 
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar/>
-        {/* <Form /> */}
-        <NewProject/>
+        <Sidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
     </>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,16 @@
-import { useState, useRef } from "react";
-
-export default function Sidebar(){
-
-    const [openForm, setOpenForm] = useState(false);
-
-    const addProject = () => {
-        
-    };
+import Button from "./Button";
+export default function Sidebar({onStartAddProject}){
 
     return(
         <>
             <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
                 <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100 flex justify-between"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" onClick={addProject}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                <Button onClick={onStartAddProject}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     Add Project
-                </button>
+                </Button>
                 <ul className="mt-8">
                     <li><em>Projects added here</em></li>
                 </ul>
