eec0e414f73c9eac6531ebfd71056dc074fbcf9f
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-04-03 17:44:09 +0200

    aggiunto Delete dei progetti già creati

--- src/App.jsx
+++ src/App.jsx
@@ -60,9 +60,22 @@ function App() {
       }
   )}
 
+  function handleDeleteProject(){
+    setProjectsState(
+      prevState => {
+        return{
+          ...prevState,
+          projects: prevState.projects.filter((project) => project.id!== prevState.selectedProjectId),
+          selectedProjectId: undefined,
+        }
+      }
+  )}
+
       const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 // console.log(projectsState);
-      let content = <Project project={selectedProject} />;
+      let content = <Project 
+      onDeleteProject={handleDeleteProject}
+      project={selectedProject} />;
 
       if(projectsState.selectedProjectId === null){
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,18 @@
 // import Input from "./Input"
-export default function Project({project}) {
+export default function Project({project, onDeleteProject}) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
         day: 'numeric'
     });
 
+
     return(
             <div className="w-[35rem] mt-16">
                 <header className="pb-4 mb-4 border-b-2 border-stone-300">
                     <div className="flex justify-between items-center">
                         <h1 className="text-stone-800 mb-4 text-5xl font-bold uppercase">{project.title}</h1>
-                        <button className="text-stone-600 hover:text-red-600">Delete</button>
+                        <button onClick={onDeleteProject} className="text-stone-600 hover:text-red-600">Delete</button>
                     </div>
                 
                 
