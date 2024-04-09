e14f2e1ac416c5447db57e78dcf5becdfc1639cc
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-04-09 17:36:55 +0200

    Clearing Tasks & Fixing Minor Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,15 @@ function App() {
   })
   }
 
-  function handleDeleteTask(){
-    
+  function handleDeleteTask(id){
+    setProjectsState(
+      prevState => {
+        return{
+          ...prevState,
+          tasks: prevState.tasks.filter((task) => task.id!== id),
+        }
+      }
+  )
   }
 
   function handleSelectProject(id){
@@ -119,6 +126,7 @@ function App() {
         onStartAddProject={handleStartAddProject}  
         projects={projectsState.projects} 
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
         />
         {content}
       </main>
